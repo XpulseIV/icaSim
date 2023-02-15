81231eddff6d36c892d96cecdf0241c896e37ab6
XpulseIV <malteaxner363@gmail.com>
2023-02-15 14:22:26 +0100

    yesyesyes

--- icaSimProj/Assets/Scripts/stigmanager.cs
+++ icaSimProj/Assets/Scripts/stigmanager.cs
@@ -4,7 +4,6 @@ using System.Threading.Tasks;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using static UnityEditor.Progress;
 
 public sealed class stigmanager : MonoBehaviour
 
